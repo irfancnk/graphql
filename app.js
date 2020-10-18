6d6b723c4b2884e55f66760521c44dfe7baede41
irfancnk <irfankaleli@yandex.com>
2020-10-18 16:44:57 +0300

    Parameter passing.

--- app.js
+++ app.js
@@ -3,32 +3,40 @@ var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-  }
-`);
+var schema = buildSchema(
+    `
+        type Query {
+            quoteOfTheDay: String
+            random: Float!
+            rollThreeDice: [Int]
+            rollDice(numDice: Int!, numSides: Int): [Int]
+        }
+    `
+);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    },
+    rollDice: (body, req) => {
+        console.log(body.numDice);
+        console.log(body.numSides);
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    }
 };
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
